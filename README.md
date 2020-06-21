cdc0c63340b030386847391c2f6f5aa855f5d75a
date_cocoa <co.yo.fo@gmail.com>
2020-06-21 18:00:45 +0900

    関数化してターミナルで実行可能に

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # SCM
 Synthetic Control Methodで遊んでみる。
 
+## 実行方法
+./SCM において以下を実行
+```zsh
+Rscripts SCM.R
+```
+./SCM/plotに結果が保存される。
+
 ## 参考文献
 https://www.jstatsoft.org/index.php/jss/article/view/v042i13/v42i13.pdf 
--- SCM.R
+++ SCM.R
@@ -13,7 +13,7 @@ library_import <- function(package_name){
     warning = function(e){ # if warning is occuerd
       print(package_name)
       print('installing and loading this package...')
-      install.packages(package_name, quiet = TRUE)
+      install.packages(package_name, quiet = TRUE, repos = "http://cran.us.r-project.org")
       library(package_name, character.only = TRUE)
       print('sucess!!')
     }
@@ -27,74 +27,157 @@ library_import <- function(package_name){
   }
 }
 
-plot_timeseries <- 
-  basque %>% 
-  filter(regionname != "Spain (Espana)") %>% # omit Spain (Espana)
-  ggplot(aes(x = year, y = gdpcap, group = regionname)) +
-  geom_line(alpha=0.5) +
-  geom_line(data = basque %>% filter(regionname=='Basque Country (Pais Vasco)'), color='red')
-ggsave(filename = './plot/plot_timeseries.png', width = 11, height = 7)
-
-# TO DO summary 
-
-##### excution ####
 library_import('tidyverse')
 library_import('Synth')
+
+plot_timeseries <- function(data, red_region, file_path = NULL, save = FALSE){
+  ###
+  # plot gdbcap data over years 
+  # input: 
+  #   data(dataframe): data
+  #   red_region(str): region you want to make red
+  #   file_path(str): file path for saving plot (default = NULL)
+  #   save(bool): wheter to save or not (default = FALSE)
+  ###
+  plot <- 
+    data %>% 
+    filter(regionname != "Spain (Espana)") %>% # omit Spain (Espana)
+    ggplot(aes(x = year, y = gdpcap, group = regionname)) +
+    geom_line(alpha=0.5) +
+    geom_line(data = basque %>% filter(regionname=='Basque Country (Pais Vasco)'), color='red')  + 
+    geom_vline(xintercept = 1970, linetype = 'dashed', color = 'purple') + 
+    ggtitle('gdpcap over years (red = Basque)')
+  
+  if(save){
+    ggsave(filename = file_path, width = 11, height = 7)
+  } else{
+    print(plot)
+  }
+}
+
+make_dataprep <- function(data, treatment_region = 'Basque Country (Pais Vasco)'){
+  ###
+  # make dataprep  
+  # input: 
+  #   data(dataframe): data
+  #   treatment_region(str): treatment region (default = 'Basque Country (Pais Vasco)')
+  # output:
+  #   dataprep_out(list): dataprep
+  ###
+  treatment.identifier <- 
+    data %>% 
+    filter(regionname == treatment_region) %>% 
+    dplyr::select(regionno) %>% 
+    .[1, 1]
+  
+  controls.identifier <- 
+    c(2:18) %>% 
+    .[. != treatment.identifier]
+  
+  dataprep_out <- dataprep(
+    foo = data,
+    predictors = c('school.illit', 
+                   'school.prim', 
+                   'school.med', 
+                   'school.high', 
+                   'school.post.high', 
+                   'invest'),
+    time.predictors.prior = 1964:1969,
+    special.predictors = list(
+      list('gdpcap', 1960:1969, 'mean'),
+      list('sec.agriculture', seq(1961, 1969, 2), 'mean'), 
+      list('sec.energy', seq(1961, 1969, 2), 'mean'),
+      list('sec.industry', seq(1961, 1969, 2), 'mean'),
+      list('sec.construction', seq(1961, 1969, 2), 'mean'),
+      list('sec.services.venta', seq(1961, 1969, 2), 'mean'),
+      list('sec.services.nonventa', seq(1961, 1969, 2), 'mean'),
+      list('popdens', 1969, 'mean')
+    ),
+    dependent = 'gdpcap',
+    unit.variable = 'regionno',
+    unit.names.variable = 'regionname',
+    time.variable = 'year',
+    treatment.identifier = treatment.identifier,
+    controls.identifier = controls.identifier,
+    time.optimize.ssr = 1960:1969,
+    time.plot = data$year %>% unique()
+  )
+  
+  return(dataprep_out)
+}
+
+plot_results <- function(dataprep_out, synth_out, file_path = NULL, save = FALSE){
+  ###
+  # plot results (synthetic gdpcap vs actual gdpcap)
+  # input: 
+  #   dataprep_out(list): return of make_dataprep()
+  #   synth_out(list): return of synth()
+  #   file_path(str): file path for saving plot (default = NULL)
+  #   save(bool): wheter to save or not (default = FALSE)
+  ###
+  plot <- 
+    tibble(
+    gdpcap = c(dataprep_out$Y0plot %*% synth_out$solution.w, dataprep_out$Y1plot),
+    kind = c(rep('synthetic', length(1955:1997)), rep('actual', length(1955:1997))),
+    year = rep(1955:1997, 2)
+  ) %>% 
+    ggplot() + 
+    geom_line(aes(x = year, y = gdpcap, color = kind)) + 
+    geom_vline(xintercept = 1970, linetype = 'dashed', color = 'purple') + 
+    ggtitle('result of SCM (actual gdpcap vs synthetic gdpcap)')
+  
+  if(save){
+    ggsave(filename = file_path, width = 11, height = 7)
+  } else{
+      print(plot)
+    }
+}
+
+plot_gap <- function(dataprep_out, synth_out, file_path = NULL, save = FALSE){
+  ###
+  # plot results (gap between actual gdpcap and synthetic gdpcap)
+  # input: 
+  #   dataprep_out(list): return of make_dataprep()
+  #   synth_out(list): return of synth()
+  #   file_path(str): file path for saving plot (default = NULL)
+  #   save(bool): wheter to save or not (default = FALSE)
+  ###
+  plot <- tibble(
+    gap = c(dataprep_out$Y1plot - dataprep_out$Y0plot %*% synth_out$solution.w),
+    year = rep(1955:1997)
+  ) %>% 
+    ggplot() +
+    geom_line(aes(x = year, y = gap)) + 
+    geom_hline(yintercept = 0, linetype = 'dashed') + 
+    ylim(-1.5, 1.5) + 
+    geom_vline(xintercept = 1970, linetype = 'dashed', color = 'purple') + 
+    ggtitle('result of SCM (gap between actual gdpcap and synthetic gdpcap)')
+  
+  if(save){
+    ggsave(filename = file_path, width = 11, height = 7)
+  } else{
+    print(plot)
+  }
+}
+
+# TO DO: summary satatistics
+
+##### excution ####
 data('basque')
+data <- basque
 
-# make data frame for synth
-dataprep_out <- dataprep(
-  foo = basque,
-  predictors = c('school.illit', 
-                 'school.prim', 
-                 'school.med', 
-                 'school.high', 
-                 'school.post.high', 
-                 'invest'),
-  time.predictors.prior = 1964:1969,
-  special.predictors = list(
-    list('gdpcap', 1960:1969, 'mean'),
-    list('sec.agriculture', seq(1961, 1969, 2), 'mean'), 
-    list('sec.energy', seq(1961, 1969, 2), 'mean'),
-    list('sec.industry', seq(1961, 1969, 2), 'mean'),
-    list('sec.construction', seq(1961, 1969, 2), 'mean'),
-    list('sec.services.venta', seq(1961, 1969, 2), 'mean'),
-    list('sec.services.nonventa', seq(1961, 1969, 2), 'mean'),
-    list('popdens', 1969, 'mean')
-  ),
-  dependent = 'gdpcap',
-  unit.variable = 'regionno',
-  unit.names.variable = 'regionname',
-  time.variable = 'year',
-  treatment.identifier = 17,
-  controls.identifier = c(2:16, 18),
-  time.optimize.ssr = 1960:1969,
-  time.plot = basque$year %>% unique()
-)
+plot_timeseries(data = data, 
+                red_region = 'Basque Country (Pais Vasco)', 
+                file_path = './plot/plot_timeseries.png',
+                save = TRUE)
 
-# search for W
-synth_out <- synth(data.prep.obj = dataprep_out)
+dataprep_out <- 
+  make_dataprep(data = data,
+                treatment_region = 'Basque Country (Pais Vasco)')
 
-tibble(
-  gdpcap = c(dataprep_out$Y0plot %*% synth_out$solution.w, dataprep_out$Y1plot),
-  kind = c(rep('synthetic', length(1955:1997)), rep('actual', length(1955:1997))),
-  year = rep(1955:1997, 2)
-) %>% 
-  ggplot() + 
-  geom_line(aes(x = year, y = gdpcap, color = kind))
+synth_out <- synth(data.prep.obj = dataprep_out) # search for W
 
-tibble(
-  gap = c(dataprep_out$Y1plot - dataprep_out$Y0plot %*% synth_out$solution.w),
-  year = rep(1955:1997)
-) %>% 
-  ggplot() +
-  geom_line(aes(x = year, y = gap)) + 
-  geom_hline(yintercept = 0, linetype = 'dashed') + 
-  ylim(-1.5, 1.5)
-
-# synth_table <- synth.tab(dataprep.res = dataprep_out, synth.res = synth_out)
-
-
-
+plot_results(dataprep_out = dataprep_out, synth_out = synth_out, file_path = './plot/plot_result.png', save = TRUE)
+plot_gap(dataprep_out = dataprep_out, synth_out = synth_out, file_path = './plot/plott_gap.png', save = TRUE)
 
 
